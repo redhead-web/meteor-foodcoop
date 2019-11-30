393a7bbc9190105ad1b9d80708d0700120904690
Sean Stanley <sean@redheadweb.nz>
2019-12-01 00:37:24 +1300

    feat: upgrade angular google places package

--- packages/angular-google-places-autocomplete/package.js
+++ packages/angular-google-places-autocomplete/package.js
@@ -1,24 +1,28 @@
 Package.describe({
   name: 'redhead:angular-google-places-autocomplete',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
+Npm.depends({
+  angular: '1.5.11',
+});
+
+Package.onUse((api) => {
   api.versionsFrom('1.2.1');
-  api.imply(['angular', 'pbastowski:angular-babel@1.0.6_1'], 'client');
+  api.imply(['ecmascript'], 'client');
   api.addFiles('angular-google-places-autocomplete.js', 'client');
   api.addFiles('angular-google-places-autocomplete.css', 'client');
   // api.export('google');
 });
 
-Package.onTest(function(api) {
+Package.onTest((api) => {
   api.use('ecmascript');
   api.use('tinytest');
   api.use('redhead:angular-google-places-autocomplete');
