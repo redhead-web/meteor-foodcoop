ed6dd0820d72fbfe1b4c5269be29cf8a4698c405
Sean Stanley <sean@redheadweb.nz>
2019-12-01 00:40:50 +1300

    fix: update script to use new template literal system

--- new.sh
+++ new.sh
@@ -7,13 +7,13 @@ touch imports/ui/components/$1/client/$1.test.js
 cat <<EOT >> imports/ui/components/$1/$1.js
 import angular from 'angular';
 
-import templateUrl from './$1.html';
+import template from './$1.html';
 
 class $1Controller {}
-const name = '$1';
+export const name = '$1';
 
 export default angular.module(name, []).component(name, {
-  templateUrl,
+  template,
   controller: $1Controller,
   controllerAs: name,
 });
